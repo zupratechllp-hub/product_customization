e0f2b8f9cab9b2da977a60145f32d0529543f6ab
PavanS1214 <pavanshinde51905@gmail.com>
2026-07-14 16:49:10 +0530

    Organisation structure tab add in zupra ERP

---
+++ product_customization/product_customization/doctype/product_customization_request/product_customization_request.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Pavan and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class ProductCustomizationRequest(Document):
+	pass
